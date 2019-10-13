d90e115e0db0882fe1ecc7e6144ca93a57b15a1d
Cameron Darren <51996720+Artlist-creator@users.noreply.github.com>
2019-10-13 06:45:18 -0700

    Finish 3st Assignment part 1

---
+++ Course-1-Java-Programming:-Solving-Problems-with-Software/Week-2/StringsThirdAssignments/src/Part_1.java
@@ -0,0 +1,149 @@
+import edu.duke.StorageResource;
+
+public class Part_1 {
+    public static int findStopCodon(String dna, int strIndex, String stpCodon) {
+        //This method returns the index of the first occurrence of stopCodon
+        //that appears past startIndex and is a multiple of 3 away from startIndex.
+        //If there is no such stopCodon, this method returns the length of the dna strand.
+        int stpIndex = dna.indexOf(stpCodon, strIndex + 3);
+        while (stpIndex != -1) {
+            if ((stpIndex - strIndex) % 3 == 0) {
+                return stpIndex;
+            }
+            stpIndex = dna.indexOf(stpCodon, stpIndex + 1);
+        }
+        return dna.length();
+    }
+
+    public static String findGene(String dna) {
+        //representing a string
+        //Find the index of the first occurrence of the start codon “ATG”. If there is no “ATG”, return the empty string.
+        int strIndex = dna.indexOf("ATG");
+        if (strIndex == -1) {
+            return "";
+        }
+        int taaIndex = (findStopCodon(dna, strIndex, "TAA"));
+        int tagIndex = (findStopCodon(dna, strIndex, "TAG"));
+        int tgaIndex = (findStopCodon(dna, strIndex, "TGA"));
+        int minIndex = Math.min(taaIndex, Math.min(tagIndex, tgaIndex));
+        if (minIndex == dna.length()) {
+            return "";
+        }
+        return dna.substring(strIndex, minIndex + 3);
+    }
+
+    public static void testFindStopCodon() {
+        String dna = "ATTATGCGTCTGTAGTGGGGC"; // strIndex = 3
+        int strIndex = 3;
+        String stpCodon = "TAG";
+        int stpIndex = findStopCodon(dna, strIndex, stpCodon);
+        System.out.println(stpIndex);
+
+        dna = "ATTATGCGTCTGTGTGGGGC"; // strIndex = 3 without stpCodon len =20
+        strIndex = 3;
+        stpCodon = "TAG";
+        stpIndex = findStopCodon(dna, strIndex, stpCodon);
+        System.out.println(stpIndex);
+
+        dna = "ATTATGCGTAGGTAGTGGGGC"; // strIndex = 3 with 1 invalid TAG
+        strIndex = 3;
+        stpCodon = "TAG";
+        stpIndex = findStopCodon(dna, strIndex, stpCodon);
+        System.out.println(stpIndex);
+    }
+
+    public static void testFindGene() {
+        String dna = "ATTTTAGATTCCGG";//dna with no start codon"ATG"
+        String gene = findGene(dna);
+        System.out.println(dna);
+        System.out.println(gene);
+
+        dna = "ATTATGTCACCACCGGGCTAGCCA";//dna with ATG and one valid stop codon TAG
+        gene = findGene(dna);
+        System.out.println(dna);
+        System.out.println(gene);
+
+        dna = "ATTATGTCACCACCGTGAGGCTAGTAACCA";//dna with ATG and multi valid stop codon TAG
+        gene = findGene(dna);
+        System.out.println(dna);
+        System.out.println(gene);
+
+        dna = "ATTATGTCACCACCGGGTAGCCA";//dna with ATG and no valid stop codon TAG
+        gene = findGene(dna);
+        System.out.println(dna);
+        System.out.println(gene);
+    }
+
+    public static void printAllGenes(String dna) {
+        int strIndex = 0;
+        while (true) {
+            strIndex = dna.indexOf("ATG", strIndex);
+            if (strIndex == -1) {
+                break;
+            }
+            int taaIndex = findStopCodon(dna, strIndex, "TAA"),
+                    tagIndex = findStopCodon(dna, strIndex, "TAG"),
+                    tgaIndex = findStopCodon(dna, strIndex, "TGA"),
+                    stpIndex = Math.min(Math.min(taaIndex, tagIndex), tgaIndex);
+            if (stpIndex == dna.length()) {
+                break;
+            }
+            String gene = dna.substring(strIndex, stpIndex + 3);
+            System.out.println(gene);
+            strIndex = stpIndex + 3;
+        }
+    }
+
+    public static int countGenes(String dna) {
+        int strIndex = 0, counter = 0;
+        while (true) {
+            strIndex = dna.indexOf("ATG", strIndex);
+            if (strIndex == -1) {
+                break;
+            }
+            int taaIndex = Part_1.findStopCodon(dna, strIndex, "TAA"),
+                    tagIndex = Part_1.findStopCodon(dna, strIndex, "TAG"),
+                    tgaIndex = Part_1.findStopCodon(dna, strIndex, "TGA"),
+                    stpIndex = Math.min(Math.min(taaIndex, tagIndex), tgaIndex);
+            if (stpIndex == dna.length()) {
+                break;
+            }
+            strIndex = stpIndex + 3;
+            counter++;
+        }
+        return counter;
+    }
+
+    public static StorageResource getAllGenes(String dna) {
+        int strIndex = 0;
+        StorageResource geneList = new StorageResource();
+        while (true) {
+            strIndex = dna.indexOf("ATG", strIndex);
+            if (strIndex == -1) {
+                break;
+            }
+            int taaIndex = findStopCodon(dna, strIndex, "TAA"),
+                    tagIndex = findStopCodon(dna, strIndex, "TAG"),
+                    tgaIndex = findStopCodon(dna, strIndex, "TGA"),
+                    stpIndex = Math.min(Math.min(taaIndex, tagIndex), tgaIndex);
+            if (stpIndex == dna.length()) {
+                break;
+            }
+            String gene = dna.substring(strIndex, stpIndex + 3);
+            geneList.add(gene);
+            strIndex = stpIndex + 3;
+        }
+        return geneList;
+    }
+
+    public static void testGetAllGenes() {
+        StorageResource geneList = getAllGenes("ATGTAGATGTCATAA");
+        for (String gene : geneList.data()) {
+            System.out.println(gene);
+        }
+    }
+
+    public static void main(String[] args) {
+        testGetAllGenes();
+    }
+}
