20ba64b2885494e2fb1e8a6ba273f91d04577e1f
Cameron Darren <51996720+Artlist-creator@users.noreply.github.com>
2019-11-15 17:42:49 -0800

    Finish assignment 3

--- Course-2-Java-Programming:-Arrays,-Lists,-and-Structured-Data/Week-1/WordLengths/src/Main.java
+++ Course-2-Java-Programming:-Arrays,-Lists,-and-Structured-Data/Week-1/WordLengths/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-
+        testCountWordLengths();
+        //I used base 0 to count but assigment used base 1
     }
 
     public static void countWordLengths(FileResource resource, int[] counts) {
@@ -13,19 +14,19 @@ public class Main {
                 boolean lastLetter = Character.isLetter(word.charAt(word.length() - 1));
                 boolean firstLetter = Character.isLetter(word.charAt(0));
                 if (firstLetter && lastLetter) {
-                    if (word.length() < counts.length) {
+                    if (word.length() - 1  > counts.length - 1) {
                         counts[counts.length - 1]++;
                     } else {
                         counts[word.length() - 1]++;
                     }
                 } else if (firstLetter || lastLetter) {
-                    if (word.length() < counts.length) {
+                    if (word.length() -2  > counts.length -1 ) {
                         counts[counts.length - 1]++;
                     } else {
                         counts[word.length() - 2]++;
                     }
                 } else if (Character.isLetter(word.charAt(1)) || Character.isLetter(word.charAt(word.length() - 2))) {
-                    if (word.length() < counts.length) {
+                    if (word.length() - 3 > counts.length - 1) {
                         counts[counts.length - 1]++;
                     } else {
                         counts[word.length() - 3]++;
@@ -37,5 +38,25 @@ public class Main {
                 }
             }
         }
+    }
+    public static void testCountWordLengths(){
+        FileResource resource = new FileResource();
+        int[] counts = new int[31];
+        countWordLengths(resource, counts);
+        for(int i : counts){
+            System.out.println(i);
+        }
+        System.out.println(indexOfMax(counts));
+    }
+
+    public static int indexOfMax(int[] values){
+        int max = 0, idx = 0 ;
+        for(int i = 0 ; i < values.length ; i++){
+            if(max < values[i]){
+                max = values[i];
+                idx = i;
+            }
+        }
+        return idx;
     }
 }
