1f7e759cbeab7c9077e9cfd635c6db9ec0f7e044
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-07-04 11:22:07 -0400

    Finish week 3 ASST 4 stage 1

---
+++ Course-5-Principles-of-Software-Design/Week-3/WordGramClassProgram/src/WordGram.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+
+public class WordGram
+{
+    private String[] myWords;
+    private int myHash;
+
+    public WordGram(String[] source, int start, int size)
+    {
+        myWords = new String[size];
+        System.arraycopy(source, start, myWords, 0, size);
+    }
+
+    public String wordAt(int index)
+    {
+        if (index < 0 || index >= myWords.length)
+        {
+            throw new IndexOutOfBoundsException("bad index in wordAt " + index);
+        }
+        return myWords[index];
+    }
+
+    public int length()
+    {
+        // DONE: Complete this method
+        return myWords.length;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringJoiner ret = new StringJoiner(" ");
+        // DONE: Complete this method
+        Arrays.stream(myWords).forEachOrdered(ret::add);
+        return ret.toString();
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        WordGram other = (WordGram) o;
+        // DONE: Complete this method
+        if (myWords.length != other.length()) return false;
+        return IntStream.range(0, myWords.length).allMatch(i -> myWords[i].equals(other.wordAt(i)));
+    }
+
+    public WordGram shiftAdd(String word)
+    {
+//        WordGram out = new WordGram(myWords, 0, myWords.length);
+        // shift all words one towards 0 and add word at the end. 
+        // you lose the first word
+        // DONE: Complete this method
+        String[] newWords = new String[myWords.length];
+        System.arraycopy(myWords, 1, newWords, 0, myWords.length - 1);
+        newWords[newWords.length - 1] = word;
+        return new WordGram(newWords, 0, newWords.length);
+    }
+
+}
