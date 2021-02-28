13e5f3b96d3efb74c5e3a860cdba9957683049a9
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-02-28 04:25:52 -0500

    Add ds for map

--- Course-9-Network-Data/src/graph/CapGraph.java
+++ Course-9-Network-Data/src/graph/CapGraph.java
@@ -1,64 +1,63 @@
-/**
- * 
- */
+/** */
 package graph;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Your name here.
- * 
- * For the warm up assignment, you must implement your Graph in a class
- * named CapGraph.  Here is the stub file.
- *
+ *     <p>For the warm up assignment, you must implement your Graph in a class named CapGraph. Here
+ *     is the stub file.
  */
 public class CapGraph implements Graph {
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#addVertex(int)
-	 */
-	@Override
-	public void addVertex(int num) {
-		// TODO Auto-generated method stub
+  public final Map<Integer, Set<Integer>> vertices;
 
-	}
+  public CapGraph() {
+    this.vertices = new HashMap<>();
+  }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#addEdge(int, int)
-	 */
-	@Override
-	public void addEdge(int from, int to) {
-		// TODO Auto-generated method stub
+  /* (non-Javadoc)
+   * @see graph.Graph#addVertex(int)
+   */
+  @Override
+  public void addVertex(int num) {
+    vertices.put(num, new HashSet<>());
+  }
 
-	}
+  /* (non-Javadoc)
+   * @see graph.Graph#addEdge(int, int)
+   */
+  @Override
+  public void addEdge(int from, int to) {
+    // TODO Auto-generated method stub
+    if (vertices.containsKey(from)) throw new IllegalArgumentException("Vertex do not exist");
+    vertices.get(from).add(to);
+  }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#getEgonet(int)
-	 */
-	@Override
-	public Graph getEgonet(int center) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  /* (non-Javadoc)
+   * @see graph.Graph#getEgonet(int)
+   */
+  @Override
+  public Graph getEgonet(int center) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#getSCCs()
-	 */
-	@Override
-	public List<Graph> getSCCs() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  /* (non-Javadoc)
+   * @see graph.Graph#getSCCs()
+   */
+  @Override
+  public List<Graph> getSCCs() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	/* (non-Javadoc)
-	 * @see graph.Graph#exportGraph()
-	 */
-	@Override
-	public HashMap<Integer, HashSet<Integer>> exportGraph() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+  /* (non-Javadoc)
+   * @see graph.Graph#exportGraph()
+   */
+  @Override
+  public HashMap<Integer, HashSet<Integer>> exportGraph() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 }
